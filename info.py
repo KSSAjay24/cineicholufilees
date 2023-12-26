36d20cbe146daf757f166991b23843c2ac2e7adc
kssajay <149785052+KSSAjay24@users.noreply.github.com>
2023-12-26 13:18:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,14 +23,14 @@ API_HASH = environ.get('API_HASH', 'f580b015eeec1f8cfb524210f426dcfa')
 if len(API_HASH) == 0:
     logging.error('API_HASH is missing, exiting now')
     exit()
-BOT_TOKEN = environ.get('BOT_TOKEN', '6651921411:AAFV0mL1LjJtS9xBpj_9i97f4C2YC7GTUz0')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6627487505:AAF79BcxApgkQlVrT-Iy7-vf-tA9JgfOpFk')
 if len(BOT_TOKEN) == 0:
     logging.error('BOT_TOKEN is missing, exiting now')
     exit()
 PORT = int(environ.get('PORT', '8080'))
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/7d39c00f753f79cfa144d.jpg.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
 
 # Bot Admins
 ADMINS = environ.get('ADMINS', '5505644708')
@@ -42,7 +42,7 @@ else:
 
 # Channels
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '').split()]
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1001839998122').split()]
+AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002104198431').split()]
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1001839998122')
 if len(LOG_CHANNEL) == 0:
     logging.error('LOG_CHANNEL is missing, exiting now')
@@ -50,7 +50,7 @@ if len(LOG_CHANNEL) == 0:
 else:
     LOG_CHANNEL = int(LOG_CHANNEL)
     
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001839998122')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1002078537393')
 if len(SUPPORT_GROUP) == 0:
     logging.error('SUPPORT_GROUP is missing, exiting now')
     exit()
@@ -71,7 +71,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/cinepicholu')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/areymawa2')
 
 # Bot settings
 AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
